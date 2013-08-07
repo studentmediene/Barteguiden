056c521430e93bcd02529b9d79195ce2b6118912
Christian Rasmussen <christian.rasmussen@me.com>
2013-08-08 01:13:43 +0200

    Renamed self to controller.

--- app/controllers/events_controller.js
+++ app/controllers/events_controller.js
@@ -13,86 +13,88 @@ var Event = app.models.Event;
 //EventsController.before(["create", "update", "destroy"], app.ensureAuthenticated);
 
 EventsController.index = function () {
-    var self = this;
+    var controller = this;
+    
     Event.findAll()
         .success(function(events) {
-            self.res.charset = "utf8"; // TODO: Move to a more central place?
-            self.res.json({ events: events });
+            controller.res.charset = "utf8"; // TODO: Move to a more central place?
+            controller.res.json({ events: events });
         })
         .error(function(err) {
-            self.next(Error.http(500, null, err));
+            controller.next(Error.http(500, null, err));
         });
 };
 
 EventsController.show = function () {
-    var self = this;
-    var id = this.req.param("id");
+    var controller = this;
+    var id = controller.req.param("id");
     
-    findEvent(id, this, function (event) {
-        self.res.charset = "utf8"; // TODO: Move to a more central place?
-        self.res.json(event);
+    findEvent(id, controller, function (event) {
+        controller.res.charset = "utf8"; // TODO: Move to a more central place?
+        controller.res.json(event);
     });
 };
 
 EventsController.create = function() {
-    var self = this;
-    var params = this.req.body;
+    var controller = this;
+    var params = controller.req.body;
     
     Event.create(Event.fromJSON(params))
         .success(function(event) {
-            self.res.charset = "utf8"; // TODO: Move to a more central place?
-            self.res.json(event);
+            controller.res.charset = "utf8"; // TODO: Move to a more central place?
+            controller.res.json(event);
         })
         .error(function(err) {
-            self.next(Error.http(500, null, err));
+            controller.next(Error.http(500, null, err));
         });
 };
 
+// TODO: Fix that an update removes fields that are not filled in...
 EventsController.update = function() {
-    var self = this;
-    var params = this.req.body;
-    var id = this.req.param("id");
+    var controller = this;
+    var params = controller.req.body;
+    var id = controller.req.param("id");
     
-    findEvent(id, this, function (event) {
+    findEvent(id, controller, function (event) {
         event.updateAttributes(Event.fromJSON(params))
             .success(function(updatedEvent) {
-                self.res.charset = "utf8"; // TODO: Move to a more central place?
-                self.res.json(updatedEvent);
+                controller.res.charset = "utf8"; // TODO: Move to a more central place?
+                controller.res.json(updatedEvent);
             })
             .error(function(err) {
-                self.next(500, null, err);
+                controller.next(500, null, err);
             });
     });
 };
 
 EventsController.destroy = function() {
-    var self = this;
-    var id = this.req.param("id");
+    var controller = this;
+    var id = controller.req.param("id");
     
-    findEvent(id, this, function (event) {
+    findEvent(id, controller, function (event) {
         event.destroy()
                 .success(function() {
-                    self.res.charset = "utf8"; // TODO: Move to a more central place?
-                    self.res.json({ ok: true });
+                    controller.res.charset = "utf8"; // TODO: Move to a more central place?
+                    controller.res.json({ ok: true });
                 })
                 .error(function(err) {
-                    self.next(500, null, err);
+                    controller.next(500, null, err);
                 });
     });
 };
 
-var findEvent = function (id, self, callback) {
+var findEvent = function (id, controller, callback) {
     Event.find(id)
         .success(function(event) {
             if (event === null) {
-                self.next(Error.http(404));
+                controller.next(Error.http(404));
                 return;
             }
             
             callback(event);
         })
         .error(function(err) {
-            self.next(500, null, err);
+            controller.next(500, null, err);
         });
 };
 
