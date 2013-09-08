450bcedad405e0e6d97204fbd25cdaf3f844cd95
Christian Rasmussen <christian.rasmussen@me.com>
2013-09-08 05:34:48 +0200

    Root controller fetches version from package.json.

--- app/controllers/root_controller.js
+++ app/controllers/root_controller.js
@@ -1,12 +1,23 @@
-/*global require, module*/
+/*global require, module, __dirname*/
 
+var fs = require("fs");
 var app = require("locomotive");
 var Controller = app.Controller;
 
 var RootController = new Controller();
 
+
+var packageFile = __dirname + "/../../package.json";
+
+
 RootController.main = function () {
-    this.res.json({ welcome: "1.0.2" });
+    this.res.json({ welcome: getVersion() });
 };
+
+function getVersion() {
+    var data = fs.readFileSync(packageFile);
+    var package = JSON.parse(data);
+    return package.version;
+}
 
 module.exports = RootController;
