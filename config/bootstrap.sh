ff91b1e6552f03c896d25a25fc82f5f58cb39bc7
Christian Rasmussen <christian.rasmussen@me.com>
2014-02-07 12:47:24 +0100

    Updated commands for import-scripts.

--- config/bootstrap.sh
+++ config/bootstrap.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+HOME=/home/vagrant
 PROJECT=/vagrant
 CONFIG=$PROJECT/config
 BACKEND=$PROJECT/backend
@@ -12,6 +13,7 @@ log () {
 
 log 'Installing common requirements...'
 apt-get update
+apt-get install -y tmux
 apt-get install -y python-software-properties # Needed for 'add-apt-repository' command
 
 log 'Installing node...'
@@ -24,13 +26,19 @@ log 'Installing nginx...'
 apt-get install -y nginx
 rm /etc/nginx/sites-enabled/default
 ln -s $CONFIG/nginx.conf /etc/nginx/sites-enabled/barteguiden.no
+service nginx restart
 
 log 'Setting up backend...'
 cd $BACKEND && npm install
 mkdir -p $BACKEND/data # Needed for sqlite database
 
-log 'Starting servers...'
-service nginx restart
-# cd $BACKEND && RESET_DB=1 npm run-script debug
+# log 'Setting up import-scripts...'
+cd $IMPORT && npm install
+cp $IMPORT/config/server.json.example $IMPORT/config/server.json
+# TODO
 
-log 'Finished bootstrapping'
+# log 'Starting servers...'
+# tmux new \; \
+#     new-window -n Backend 'cd $BACKEND && RESET_DB=1 npm run-script debug'
+
+log 'Finished bootstrapping!'
