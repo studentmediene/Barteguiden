a41fa3d7fe8d6d48f335d85d4f25cd4ba33f61a1
Anna Kastet <annakastet@gmail.com>
2014-11-01 17:29:13 +0100

    Added Event Service

---
+++ app/scripts/services/eventservice.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name barteguidenMarkedsWebApp.eventService
+ * @description
+ * # eventService
+ * Factory in the barteguidenMarkedsWebApp.
+ */
+angular.module('barteguidenMarkedsWebApp.services')
+  .service('eventService', function ($http) {
+    var baseURL = 'http://barteguiden.no/v2/';
+    //$http.get(baseURL + 'events');
+    return {
+      getAllEvents: function() {
+        //just get the json file locally now
+        return $http.get(baseURL + 'events');
+      },
+      getEventById: function(id) {
+        return $http.get(baseURL + 'events/' + id.toString());
+      }
+    };
+  });
+
