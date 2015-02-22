0a38f8f3c6ade2dfbe9dfbbb8b9cbbad84fdfa74
Martin Thoresen <martinhath@gmail.com>
2015-02-22 18:10:43 +0100

    Added notes, and fixed image path

--- controllers/image.js
+++ controllers/image.js
@@ -6,15 +6,19 @@ exports.postImage = function(req, res){
     var fstream;
     req.pipe(req.busboy);
     req.busboy.on('file', function(fieldname, file, filename) {
-        console.log('chillern med '+filename);
-        fstream = fs.createWriteStream(__dirname+'/../uploads/'+filename);
+        fstream = fs.createWriteStream(path.resolve('./uploads/'+filename));
         file.pipe(fstream);
         fstream.on('close', function() {
+            // TODO: return file url.
             res.json({path:'hva med nei.png'});
         });
     });
-}
+};
 
 exports.getImages = function(req, res){
+    // TODO: List all urls available
     res.sendfile(path.resolve('uploads/'));
-}
+};
+
+// TODO: 
+// exports.getImage = function(req, res){};
