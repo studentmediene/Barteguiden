228fd5685a49b7cb35ccc96bdcfdb2fd03ed0cc9
Eirik Rognø <eirik.rogno@gmail.com>
2015-04-19 22:14:25 +0200

    Added ability to place marker on map and verification for the input data

--- app/scripts/controllers/venue.js
+++ app/scripts/controllers/venue.js
@@ -10,12 +10,54 @@
 angular.module('barteguidenMarkedsWebApp')
   .controller('VenueCtrl', function ($scope) {
 
+    $scope.venue = {};
+    $scope.clicked = false;
+
+    $scope.clickedMarker = {
+      id:0,
+      coords: {
+        latitude: 63.422634,
+        longitude:10.394697
+      }
+    };
+
     $scope.map = {
       center: {
         latitude: 63.43,
         longitude: 10.39
       },
-      zoom: 13
+      zoom: 13,
+      events: {
+        tilesloaded: function (map, eventName, originalEventArgs) {
+          //map is truly ready then this callback is hit
+        },
+        click: function (mapModel, eventName, originalEventArgs) {
+
+          var e = originalEventArgs[0];
+          var lat = e.latLng.lat(),
+            lon = e.latLng.lng();
+          $scope.clickedMarker = {
+            id: 0,
+            title: 'You clicked here ' + 'lat: ' + lat + ' lon: ' + lon,
+            latitude: lat,
+            longitude: lon
+          };
+          $scope.venue.latitude = lat;
+          $scope.venue.longitude = lon;
+          $scope.clicked = true;
+          //scope apply required because this event handler is outside of the angular domain
+          $scope.$apply();
+        }
+      }
     };
+
+    $scope.hasClicked = function(){
+      return $scope.clicked;
+    };
+
+    $scope.submit = function() {
+      console.log("Success");
+    };
+
 
   });
