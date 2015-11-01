71ee64ad6f4e32dc869549d6c64a8bf28a8ca7b0
Anna Kastet <annakastet@gmail.com>
2015-11-01 18:33:29 +0100

    Hide menu bar when not logged in

--- app/scripts/controllers/menu.js
+++ app/scripts/controllers/menu.js
@@ -1,5 +1,10 @@
 angular.module('barteguidenMarkedsWebApp')
-  .controller('MenuCtrl', function ($scope, $location) {
+  .controller('MenuCtrl', function ($scope, $location, Auth) {
     $scope.isCurrentView = function(e) {
       return e === $location.path();
-    }});
+    };
+
+    $scope.isLoggedIn = function() {
+      return Auth.isLoggedIn();
+    }
+  });
