2e5e91cc9d6d0b6586aa1b995179505f8edbe265
Andreas Drivenes <andreas.drivenes@gmail.com>
2016-02-22 00:00:43 +0100

    Fetch events from Facebook Graph API
    Places: Standup Trondheim, Brukbar/Blæst, Antikvariatet, Byscenen
    Familien.
    Todo: Where to fetch prices and age limit?

--- server/import/jobs.js
+++ server/import/jobs.js
@@ -3,7 +3,8 @@ var feeds = [
     require('./import_scripts/samfundet.js'),
     require('./import_scripts/uka.js'),
     require('./import_scripts/google_drive.js'),
-    require('./import_scripts/trdevents.js')
+    require('./import_scripts/trdevents.js'),
+    require('./import_scripts/facebook.js')
 ];
 
 module.exports = new CronJob('00 30 11 * * 0-6', function(){
